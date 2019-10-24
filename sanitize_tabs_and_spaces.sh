475b8786072bc7c9a73a71e172d30dfa18797daf
Csaba Hruska <csaba.hruska@gmail.com>
2019-10-24 18:58:35 +0200

    remove script

--- sanitize_tabs_and_spaces.sh
+++
@@ -1,7 +0,0 @@
-for ext in cabal hs yaml c h ll s; do
-  # removes trailing spaces
-  find . -name "*.$ext" -type f -print0 | xargs -0 sed -i 's/[[:space:]]*$//'
-
-  # expand tabs to spaces
-  find . -name "*.$ext" ! -type d -exec bash -c 'expand "$0" > /tmp/e && mv /tmp/e "$0"' {} \;
-done
